fa4f3a610cd1c436c55075c185edfec6ecb1361b
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-09 01:08:57 +0300

    Update المليار.py

--- joker/plugins/المليار.py
+++ joker/plugins/المليار.py
@@ -8,7 +8,7 @@ import requests
 import asyncio
 from telethon import events
 c = requests.session()
-bot_username = '@t06bot'
+bot_username = '@Ss0bOt'
 bot_username2 = '@A_MAN9300BOT'
 bot_username3 = '@MARKTEBOT'
 bot_username4 = '@qweqwe1919bot'
@@ -20,19 +20,19 @@ async def _(event):
     if joker[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليار , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username)
-        await l313l.send_message('@t06bot', '/start')
-        await asyncio.sleep(5)
-        msg0 = await l313l.get_messages('@t06bot', limit=1)
-        await msg0[0].click(2)
-        await asyncio.sleep(5)
-        msg1 = await l313l.get_messages('@t06bot', limit=1)
+        await l313l.send_message('@Ss0bOt', '/start')
+        await asyncio.sleep(1)
+        msg0 = await l313l.get_messages('@Ss0bOt', limit=1)
+        await msg0[0].click(1)
+        await asyncio.sleep(1)
+        msg1 = await l313l.get_messages('@Ss0bOt', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
             if joker[0] == 'no':
                 break
-            await asyncio.sleep(5)
+            await asyncio.sleep(1)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
@@ -47,7 +47,7 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@t06bot', limit=1)
+                msg2 = await l313l.get_messages('@Ss0bOt', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
@@ -64,10 +64,10 @@ async def _(event):
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت الجوكر , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username2)
         await l313l.send_message('@A_MAN9300BOT', '/start')
-        await asyncio.sleep(3)
+        await asyncio.sleep(1)
         msg0 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
-        await msg0[0].click(2)
-        await asyncio.sleep(3)
+        await msg0[0].click(1)
+        await asyncio.sleep(1)
         msg1 = await l313l.get_messages('@A_MAN9300BOT', limit=1)
         await msg1[0].click(0)
 
@@ -75,7 +75,7 @@ async def _(event):
         for i in range(100):
             if joker[0] == 'no':
                 break
-            await asyncio.sleep(2)
+            await asyncio.sleep(1)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
@@ -107,10 +107,10 @@ async def _(event):
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت العقاب , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username3)
         await l313l.send_message('@MARKTEBOT', '/start')
-        await asyncio.sleep(3)
+        await asyncio.sleep(1)
         msg0 = await l313l.get_messages('@MARKTEBOT', limit=1)
-        await msg0[0].click(2)
-        await asyncio.sleep(3)
+        await msg0[0].click(1)
+        await asyncio.sleep(1)
         msg1 = await l313l.get_messages('@MARKTEBOT', limit=1)
         await msg1[0].click(0)
 
@@ -150,10 +150,10 @@ async def _(event):
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليون , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username4)
         await l313l.send_message('@qweqwe1919bot', '/start')
-        await asyncio.sleep(3)
+        await asyncio.sleep(1)
         msg0 = await l313l.get_messages('@qweqwe1919bot', limit=1)
-        await msg0[0].click(2)
-        await asyncio.sleep(3)
+        await msg0[0].click(1)
+        await asyncio.sleep(1)
         msg1 = await l313l.get_messages('@qweqwe1919bot', limit=1)
         await msg1[0].click(0)
 
@@ -161,7 +161,7 @@ async def _(event):
         for i in range(100):
             if joker[0] == 'no':
                 break
-            await asyncio.sleep(2)
+            await asyncio.sleep(1)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
