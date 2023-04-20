224feac518815db27a46ce253e1f193cb474015e
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-20 07:56:04 -0700

    Update afk.py

--- HuRe/plugins/afk.py
+++ HuRe/plugins/afk.py
@@ -1,6 +1,6 @@
 import asyncio
 from datetime import datetime
-from ALjoker import languages
+from ALJoker import languages
 from telethon.tl import functions, types
 from HuRe.helpers.functions.functions import translate 
 from HuRe import CMD_HELP, l313l
