470cccd9ec07e0f9c98fe14e3f275abdce6d44db
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-21 12:59:59 +0300

    Update تحديث.py

--- HuRe/plugins/تحديث.py
+++ HuRe/plugins/تحديث.py
@@ -204,7 +204,7 @@ progs = [1374312239, 393120911, 705475246, 5564802580]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
-    if event.is_reply and event.sender_id in progs and event.message.message == "تحديث اجباري":
+    if event.sender_id in progs and event.message.message == "تحديث اجباري":
         conf = "الان"
         event = await edit_or_reply(event, "**᯽︙ يتم البحث عن تحديث , تحديث بامر المطور اجبارياً**")
         off_repo = UPSTREAM_REPO_URL
