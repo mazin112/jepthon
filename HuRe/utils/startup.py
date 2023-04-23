ef2e40d2bd3b49e615f6c0704bac46a45bc25d34
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-23 22:34:50 +0300

    Update startup.py

--- HuRe/utils/startup.py
+++ HuRe/utils/startup.py
@@ -28,7 +28,7 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from .pluginmanager import load_module
 from .tools import create_supergroup
 LOGS = logging.getLogger("HuRe")
-
+##Reda hands here
 cmdhr = Config.COMMAND_HAND_LER
 bot = l313l
 ENV = bool(os.environ.get("ENV", False))
