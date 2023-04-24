f85afd351598ec5f7ec267050a7474a41ab2fa01
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-24 11:10:18 +0300

    Update tools.py

--- HuRe/utils/tools.py
+++ HuRe/utils/tools.py
@@ -4,8 +4,11 @@ from telethon.tl.functions.channels import (
     InviteToChannelRequest,
 )
 from telethon.tl.functions.messages import ExportChatInviteRequest
-from telethon.tl.types import PeerUser
+from telethon.tl.types import PeerChannel
 
+from ..core.logger import logging
+
+LOGS = logging.getLogger("super_group")
 
 async def create_supergroup(group_name, client, botusername, descript, photo):
     try:
@@ -16,7 +19,10 @@ async def create_supergroup(group_name, client, botusername, descript, photo):
                 megagroup=True,
             )
         )
-        created_chat_id = result.chats[0].id
+        created_chat_idd = result.chats[0].id
+        created_idd = result.chats[0]
+        LOGS.error(str(created_idd)) 
+        created_chat_id = await client.get_entity(PeerChannel(created_chat_idd))
         result = await client(
             ExportChatInviteRequest(
                 created_chat_id
@@ -37,6 +43,5 @@ async def create_supergroup(group_name, client, botusername, descript, photo):
             )
     except Exception as e:
         return "error", str(e)
-    if not str(created_chat_id).startswith("-100"):
-        created_chat_id = int("-100" + str(created_chat_id))
+    
     return result, created_chat_id
