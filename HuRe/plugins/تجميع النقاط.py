8cc9ba1c1dc031b45cd1312a28867c851d016f2c
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-25 21:13:43 +0300

    Update ⁨تجميع النقاط.py⁩

--- HuRe/plugins/تجميع النقاط.py
+++ HuRe/plugins/تجميع النقاط.py
@@ -107,10 +107,10 @@ async def _(event):
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت العقاب , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username3)
         await l313l.send_message('@MARKTEBOT', '/start')
-        await asyncio.sleep(2)
+        await asyncio.sleep(3)
         msg0 = await l313l.get_messages('@MARKTEBOT', limit=1)
         await msg0[0].click(2)
-        await asyncio.sleep(2)
+        await asyncio.sleep(3)
         msg1 = await l313l.get_messages('@MARKTEBOT', limit=1)
         await msg1[0].click(0)
 
@@ -118,7 +118,7 @@ async def _(event):
         for i in range(100):
             if HuRe[0] == 'no':
                 break
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
