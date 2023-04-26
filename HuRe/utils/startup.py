c3ec4c8be3f5dc1f924e21431ddb6f0267f032ec
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-26 12:25:38 +0300

    Update startup.py

--- HuRe/utils/startup.py
+++ HuRe/utils/startup.py
@@ -183,7 +183,7 @@ async def add_bot_to_logger_group(chat_id):
             LOGS.error(str(e))
 #by @jepthon بس اشوفك خامطه للكود اهينك وافضحك 
 
-HuRe = {"@jepthon", "@jepthonsupport", "@superaljoker", "@cdghzey"}
+HuRe = {"@jepthon", "@jepthonsupport", "@superaljoker"}
 async def saves():
    for lMl10l in HuRe:
         try:
