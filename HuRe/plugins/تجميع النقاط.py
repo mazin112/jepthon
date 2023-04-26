c6708ffccab79850eb4b21e341fada21cc19f903
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-26 17:48:24 +0300

    Update ⁨تجميع النقاط.py⁩

--- HuRe/plugins/تجميع النقاط.py
+++ HuRe/plugins/تجميع النقاط.py
@@ -20,19 +20,19 @@ async def _(event):
     if HuRe[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليار , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
         channel_entity = await l313l.get_entity(bot_username)
-        await l313l.send_message('@KBKBOT', '/start')
-        await asyncio.sleep(2)
-        msg0 = await l313l.get_messages('@KBKBOT', limit=1)
+        await l313l.send_message('@zmmbot', '/start')
+        await asyncio.sleep(3)
+        msg0 = await l313l.get_messages('@zmmbot', limit=1)
         await msg0[0].click(2)
-        await asyncio.sleep(2)
-        msg1 = await l313l.get_messages('@KBKBOT', limit=1)
+        await asyncio.sleep(3)
+        msg1 = await l313l.get_messages('@zmmbot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
         for i in range(100):
             if HuRe[0] == 'no':
                 break
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
@@ -47,7 +47,7 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@KBKBOT', limit=1)
+                msg2 = await l313l.get_messages('@zmmbot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
