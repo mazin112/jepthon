e627e294095d38e418289c864058b79aa036dce8
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-05-15 13:27:03 +0300

    Update اوامري.py

--- HuRe/plugins/اوامري.py
+++ HuRe/plugins/اوامري.py
@@ -90,6 +90,8 @@ async def repo(event):
 @l313l.tgbot.on(CallbackQuery(data=re.compile(rb"l313l0")))
 @check_owner
 async def _(event):
+    await event.reply(str(event))
+    print(event)
     buttons = [
     [
       Button.inline("التالي", data="jrzst"),
