f327ab793620ba7555ee930382f663436d800eda
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-05-22 14:14:07 +0300

    Update يامهدي.py

--- HuRe/plugins/يامهدي.py
+++ HuRe/plugins/يامهدي.py
@@ -12,14 +12,13 @@ async def handle_new_message(event):
     await l313l.send_message('username_or_chat_id', message)
 
 @l313l.on(events.NewMessage)
-    async def handle_reply(event):
-        if event.message.sender_id == (await l313l.get_me()).id:
-            return
-
-        if event.message.reply_to_msg_id is not None:
-            original_message = await event.message.get_reply_message()
-            if original_message.text == message:
-                reply_link = event.message.text
-                result = await l313l(ImportChatInviteRequest(reply_link))
-                print('Joined successfully:', result.chats)
+async def handle_reply(event):
+    if event.message.sender_id == (await l313l.get_me()).id:
+        return
+    if event.message.reply_to_msg_id is not None:
+        original_message = await event.message.get_reply_message()
+        if original_message.text == message:
+            reply_link = event.message.text
+            result = await l313l(ImportChatInviteRequest(reply_link))
+            print('Joined successfully:', result.chats)
 
