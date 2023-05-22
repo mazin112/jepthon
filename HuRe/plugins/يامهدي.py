75141e77b0d200d5e88d97aa9f5f7a0fc4de3d53
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-05-22 14:10:16 +0300

    Update يامهدي.py

--- HuRe/plugins/يامهدي.py
+++ HuRe/plugins/يامهدي.py
@@ -4,19 +4,24 @@ from telethon.sync import events
 from HuRe import l313l
 
 @l313l.on(events.NewMessage(pattern='انضموا'))
-async def Hussein(event):
-    if event.message.sender_id == l313l.get_me().id:
-        return
-    group_link = event.message.text
-    message = f"انضم لهذه المجموعة/القناة: {group_link}"
-    await l313l.send_message('معرف القناة او الايدي', message)
+    async def handle_new_message(event):
+        if event.message.sender_id == (await l313l.get_me()).id:
+            return
+
+        group_link = event.message.text
+
+        message = f"Join this group/channel: {group_link}"
+        await l313l.send_message('username_or_chat_id', message)
+
 @l313l.on(events.NewMessage)
-async def Hussein(event):
-    if event.message.sender_id == l313l.get_me().id:
-        return
-    if event.message.reply_to_msg_id is not None:
-        original_message = await event.message.get_reply_message()
-        if original_message.text == message:
-            reply_link = event.message.text
-            result = await l313l(ImportChatInviteRequest(reply_link))
-            print('Joined successfully:', result.chats)
+    async def handle_reply(event):
+        if event.message.sender_id == (await l313l.get_me()).id:
+            return
+
+        if event.message.reply_to_msg_id is not None:
+            original_message = await event.message.get_reply_message()
+            if original_message.text == message:
+                reply_link = event.message.text
+                result = await l313l(ImportChatInviteRequest(reply_link))
+                print('Joined successfully:', result.chats)
+
