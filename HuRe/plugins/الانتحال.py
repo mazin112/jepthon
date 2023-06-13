695eff26d1b3a225d87cbe380513bcde7caa2dd1
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-13 20:42:40 +0300

    Update ⁨الانتحال.py⁩

--- HuRe/plugins/الانتحال.py
+++ HuRe/plugins/الانتحال.py
@@ -43,7 +43,6 @@ async def _(event):
         return await edit_delete(event, "**لا تحاول تنتحل المطورين ادبسز!**")
     user_id = replied_user.id
     profile_pics = await event.client.get_profile_photos(user_id)
-    profile_pics = reversed(profile_pics)
     for photo in profile_pics:
         photo_file = await event.client.download_media(photo, Config.TEMP_DIR)
         await event.client(functions.photos.UploadProfilePhotoRequest(await event.client.upload_file(photo_file)))
