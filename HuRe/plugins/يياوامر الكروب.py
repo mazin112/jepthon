291aaeb5a4ace61b0ae1833be1eae99d58aefeca
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-15 20:10:13 +0300

    Update ⁨يياوامر الكروب.py⁩

--- HuRe/plugins/يياوامر الكروب.py
+++ HuRe/plugins/يياوامر الكروب.py
@@ -1117,7 +1117,7 @@ async def list_banned_names(event):
     banned_names_str = "\n- ".join(banned_names) if banned_names else "**᯽︙ لا توجد أسماء ممنوعة حاليًا.**"
     await event.reply(f"**᯽︙ الأسماء الممنوعة حاليًا:**\n- {banned_names_str}")
 
-@l313l.ar_cmd(pattern=r"الذكاء(.*)")
+@l313l.ar_cmd(pattern=r"(الذكاء|ذكاء)(.*)")
 async def hussein(event):
     await event.edit("**᯽︙ جارِ الجواب على سؤالك انتظر قليلاً ...**")
     text = event.pattern_match.group(1).strip()
