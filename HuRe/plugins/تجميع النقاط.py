c4ed3513d6dbe108f73627e0019d91712e741655
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-16 16:54:34 +0300

    Update ⁨تجميع النقاط.py⁩

--- HuRe/plugins/تجميع النقاط.py
+++ HuRe/plugins/تجميع النقاط.py
@@ -243,7 +243,7 @@ async def hussein(event):
     else:
         await event.edit("**᯽︙ هذا الأمر يمكن استخدامه فقط في المجموعات!**")
 
-@l313l.on(admin_cmd(pattern="استثمار وعد (\d+)"))
+@l313l.on(admin_cmd(pattern="استثمار وعد"))
 async def hussein(event):
     if event.is_group:
         message = event.pattern_match.group(1).strip()
@@ -253,20 +253,20 @@ async def hussein(event):
             its_hussein_status = gvarstatus("its_hussein")
             if its_hussein_status != "True":
                 addgvar("its_hussein", "True")
-                await reham_english(event, message)
+                await Reham_english(event, message)
             else:
                 await event.edit("**استثمار وعد قيد التشغيل بالفعل!**")
         else:
-            await event.edit("**يرجى كتابة رقم الاستثمار مع الامر!**")
+            await event.edit("**يرجى كتابة رقم الاستثمار مع الأمر!**")
     else:
         await event.edit("**هذا الأمر يمكن استخدامه فقط في المجموعات!**")
-async def reham_english(event, message):
+async def Reham_english(event, message):
     its_hussein_status = gvarstatus("its_hussein")
     if its_hussein_status == "True":
         if re.match("^\d+$", message):
             await event.respond(f"استثمار {message}")
             await asyncio.sleep(660)
-            await reham_english(event, message)
+            await Reham_english(event, message)
         else:
             await event.edit("**تنبيه: يجب أن يحتوي رقم الاستثمار على أرقام فقط!**")
 @l313l.on(admin_cmd(pattern="ايقاف استثمار وعد"))
