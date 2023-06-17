22e222a1d2042c759071bb8f285cb805f72f133f
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-17 14:31:55 +0300

    Update فلم.py

--- HuRe/plugins/فلم.py
+++ HuRe/plugins/فلم.py
@@ -52,8 +52,7 @@ async def rfilm(event):
         for video in promo_videos:
             buttons.append(Button.url("مشاهدة الفيديو", f"https://www.youtube.com/watch?v={video['key']}"))
     await event.delete()
-    await l313l.send_file(
-        event.chat_id,
+    await event.respond(
         moviet,
         buttons=buttons,
         file=moviep,
