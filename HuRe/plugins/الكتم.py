0081bd64295a1f6f92b75924e362e45ca1bba424
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-19 00:25:29 +0300

    Update ⁨الكتم.py⁩

--- HuRe/plugins/الكتم.py
+++ HuRe/plugins/الكتم.py
@@ -6,6 +6,7 @@ from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChatBannedRights
+from telethon.utils import get_display_name
 
 from HuRe import l313l
 
