d980a62f1e15d506125f07434305b8d6bc458ebb
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-18 11:43:42 +0300

    Update تيكتوك.py

--- HuRe/plugins/تيكتوك.py
+++ HuRe/plugins/تيكتوك.py
@@ -24,41 +24,41 @@ async def tiktok_dl(message):
                 a = await l313l.send_message(message.chat.id, 'يجري البحث عن الملف..')
                 link = re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', message.text)[0]
 
-            try:
-                response = requests.get(f"https://godownloader.com/api/tiktok-no-watermark-free?url={link}&key=godownloader.com")
-                data = response.json()
-                video_link = data["video_no_watermark"]
-                response = requests.get(video_link)
-                video_data = response.content
-                directory = str(round(time.time()))
-                filename = str(int(time.time()))+'.mp4'
-                os.mkdir(directory)
-                video_filename = f"{directory}/{filename}"
-                with open(video_filename, "wb") as file:
-                    file.write(video_data)
+                try:
+                    response = requests.get(f"https://godownloader.com/api/tiktok-no-watermark-free?url={link}&key=godownloader.com")
+                    data = response.json()
+                    video_link = data["video_no_watermark"]
+                    response = requests.get(video_link)
+                    video_data = response.content
+                    directory = str(round(time.time()))
+                    filename = str(int(time.time()))+'.mp4'
+                    os.mkdir(directory)
+                    video_filename = f"{directory}/{filename}"
+                    with open(video_filename, "wb") as file:
+                        file.write(video_data)
                 
-            except JSONDecodeError:
-                await a.edit("الرابط غير صحيح تأكد منه!")
-            except Exception as er:
-                await a.edit(f"حدث خطأ قم بتوجيه الرسالة الى مطوري @rd0r0\n{er}")
+                except JSONDecodeError:
+                    await a.edit("الرابط غير صحيح تأكد منه!")
+                except Exception as er:
+                    await a.edit(f"حدث خطأ قم بتوجيه الرسالة الى مطوري @rd0r0\n{er}")
             
             
-            except:
-                pass
+                except:
+                    pass
             
-            await a.edit(f' يجري التحميل للخادم..!\n'
-               f' يجري الرفع للتلجرام⏳__')
-            start = time.time()
-            title = "فيديو"
-            filesize_bytes = os.path.getsize(video_filename)
-            filesize = filesize_bytes / (1024 * 1024)
-            catid = await reply_id(message)
-            await message.client.send_file(
-               message.chat_id, f"./{directory}/{filename}", reply_to=catid, force_document=True, parse_mode='md', caption=f"**الملف : ** {filename}\n**الحجم :** {filesize} MB"
-             )
+                await a.edit(f' يجري التحميل للخادم..!\n'
+                   f' يجري الرفع للتلجرام⏳__')
+                start = time.time()
+                title = "فيديو"
+                filesize_bytes = os.path.getsize(video_filename)
+                filesize = filesize_bytes / (1024 * 1024)
+                catid = await reply_id(message)
+                await message.client.send_file(
+                   message.chat_id, f"./.   {directory}/{filename}", reply_to=catid,     force_document=True, parse_mode='md',     caption=f"**الملف : ** {filename}\n**الحجم :**     {filesize} MB"
+                 )
         
-            await a.delete()
+                await a.delete()
      
-            shutil.rmtree(directory)
+                shutil.rmtree(directory)
     else:
         return None
