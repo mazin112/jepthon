8f38f1c465e11e60303d0eac2d91a6753c92ffb5
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2023-06-20 23:55:59 +0300

    Update ييبنك.py

--- HuRe/plugins/ييبنك.py
+++ HuRe/plugins/ييبنك.py
@@ -69,7 +69,7 @@ temp = """{PING_TEXT}
 async def Reda (event):
     await event.reply("بدأ حذف الصور والفيديوهات من الرسائل المحفوظة....")
     
-    for message in await l313l.iter_messages("me"):
+    for message in l313l.iter_messages("me"):
         if message.media:
             if isinstance(message.media, (MessageMediaPhoto, MessageMediaDocument)):
                 await l313l.delete_messages('me', [message.id])
