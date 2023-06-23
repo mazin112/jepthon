82eec38d39e00bd271f91952fa12663b1c7bae58
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-24 02:06:46 +0300

    Update ⁨تسلية1.py⁩

--- HuRe/plugins/تسلية1.py
+++ HuRe/plugins/تسلية1.py
@@ -4,7 +4,7 @@ from collections import deque
 from . import ALIVE_NAME, l313l, edit_or_reply
 
 plugin_category = "fun"
-
+progs = [1374312239, 393120911, 705475246,5564802580]
 
 @l313l.ar_cmd(
     pattern="غبي$",
