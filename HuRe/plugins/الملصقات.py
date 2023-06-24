d3f3604eeb0678fb049d475839b96ea0cc7b9d05
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-24 22:26:08 +0300

    Update ⁨الملصقات.py⁩

--- HuRe/plugins/الملصقات.py
+++ HuRe/plugins/الملصقات.py
@@ -492,6 +492,44 @@ async def HuRepkg(_):
     await edit_or_reply(
         _, f"**- تم اخذ الحزمة بنجاح ✓ \nالحزمة  → [اضغط هنا](https://t.me/addstickers/{HuRe_Jep.set.short_name})**")
 
+@l313l.on(admin_cmd(pattern="حزمه"))
+async def JokSte(_):
+    Jep = await _.get_reply_message()
+    if not Jep:
+        return await edit_or_reply(_, "**- يجب عليك الرد على حزمة.**")
+    if len(_.text) > 9:
+        _packname = _.text.split(" ", maxsplit=1)[1]
+    else:
+        _packname = f"{_.sender_id}"
+    _id = Jep.media.document.attributes[1].stickerset.id
+    _hash = Jep.media.document.attributes[1].stickerset.access_hash
+    _get_stiks = await _.client(functions.messages.GetStickerSetRequest(types.InputStickerSetID(id=_id, access_hash=_hash), hash=0))
+    stiks = []
+    for i in _get_stiks.documents:
+        mul = get_input_document(i)
+        stiks.append(
+            types.InputStickerSetItem(
+                document=mul,
+                emoji=(i.attributes[1]).alt,
+            )
+        )
+    try:
+        short_name = (await _.client(SuggestShortNameRequest(_packname))).short_name
+        HuRe_Jep = await bot(
+            functions.messages.CreateChatRequest(
+                users=[_.sender_id],
+                title=_packname,
+                random_id=random.randint(1, 1000000000),
+                draft=get_input_draft(stiks),
+                flags=1,
+            )
+        )
+    except BaseException as er:
+        LOGS.exception(er)
+        return await edit_or_reply(_, str(er))
+    await edit_or_reply(
+        _, f"**- تم اخذ الحزمة بنجاح ✓ \nالحزمة  → [اضغط هنا](https://t.me/addstickers/{HuRe_Jep.set.short_name})**")
+        
 @l313l.ar_cmd(
     pattern="معلومات_الملصق$",
     command=("معلومات_الملصق", plugin_category),
