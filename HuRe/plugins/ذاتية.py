2e1e5a08261129780344a99971225c283e8a9d64
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-30 17:49:09 +0300

    Update ⁨ذاتية.py⁩

--- HuRe/plugins/ذاتية.py
+++ HuRe/plugins/ذاتية.py
@@ -1,7 +1,7 @@
 from HuRe import l313l
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 import os
-from telethon import types
+from telethon import events
 from HuRe import *
 
 @l313l.on(admin_cmd(pattern="(جلب الصورة|جلب الصوره|ذاتيه|ذاتية|حفظ)"))
