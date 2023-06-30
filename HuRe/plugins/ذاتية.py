80b7ce256713479719f4edf2aebb420c582e1ab8
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-06-30 17:14:12 +0300

    Update ⁨ذاتية.py⁩

--- HuRe/plugins/ذاتية.py
+++ HuRe/plugins/ذاتية.py
@@ -40,10 +40,12 @@ async def Reda_Is_Here(event):
 @l313l.ar_cmd(incoming=True)
 async def reda(event):
     if gvarstatus("savepicforme"):
-        if event.is_private:
+        if event.is_private and event.media_unread:
             if event.photo or event.video_note or (event.video and event.video.attributes and any(attr for attr in event.video.attributes if isinstance(attr, types.DocumentAttributeVideo))) or (event.media.document and not event.media.document.attributes):
                 pic = await event.download_media()
                 sender_Joker = event.sender.first_name
+                sender_username = event.sender.username
+                profile_Joker = f"https://t.me/{sender_username}"
                 await bot.send_file(
                     "me",
                     pic,
@@ -52,7 +54,8 @@ async def reda(event):
                     - غير مبري الذمه اذا استخدمت الامر للابتزاز
                     - CH: @Jepthon
                     - Dev: @rd0r0
-                    - المرسل: {sender_Joker}
-                    """
+                    - المرسل: [{sender_Joker}]({profile_Joker})
+                    """,
+                    parse_mode="Markdown"
                 )
                 os.remove(pic)
