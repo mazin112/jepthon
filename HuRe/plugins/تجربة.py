0bf8f68145364b4e8ac3175704314dec5eca23c0
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-01 14:38:35 +0300

    Create تجربة.py

---
+++ HuRe/plugins/تجربة.py
@@ -0,0 +1,124 @@
+import asyncio
+import base64
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+
+from HuRe import l313l
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.tools import media_type
+from ..helpers.utils import _catutils
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID
+
+async def spam_function(event, HuRe, l313l, sleeptimem, sleeptimet, DelaySpam=False):
+
+    counter = int(l313l[0])
+    spam_message = ' '.join(l313l[1:])
+
+    if len(l313l) >= 2:
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            if event.reply_to_msg_id:
+                await HuRe.reply(spam_message)
+            else:
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and HuRe.media:
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            HuRe = await event.client.send_file(
+                event.chat_id, HuRe, caption=HuRe.text
+            )
+            await _catutils.unsavegif(event, HuRe)
+            await asyncio.sleep(sleeptimem)
+        if BOTLOG:
+            if DelaySpam is not True:
+                if event.is_private:
+                    await event.client.send_message(
+                        BOTLOG_CHATID,
+                        "**⌔∮ التڪرار  **\n"
+                        + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **عدد المرات مع الرسالة أدناه**",
+                    )
+                else:
+                    await event.client.send_message(
+                        BOTLOG_CHATID,
+                        "**⌔∮ التڪرار  **\n"
+                        + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **مع** {counter} **عدد المرات مع الرسالة أدناه**",
+                    )
+            elif event.is_private:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التكرار الوقتي **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **عدد المرات مع الرسالة أدناه مع التأخير** {sleeptimet} ** الثواني **",
+                )
+            else:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التكرار الوقتي **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **مع** {counter} **عدد المرات مع الرسالة أدناه مع التأخير** {sleeptimet} ** الثواني **",
+                )
+
+            HuRe = await event.client.send_file(BOTLOG_CHATID, HuRe)
+            await _catutils.unsavegif(event, HuRe)
+        return
+    elif event.reply_to_msg_id and HuRe.text:
+        spam_message = HuRe.text
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    else:
+        return
+    if DelaySpam is not True:
+        if BOTLOG:
+            if event.is_private:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التڪرار  **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **رسائل ال   :** \n"
+                    + f"⌔∮ `{spam_message}`",
+                )
+            else:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التڪرار  **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع** {counter} **رسائل الـ   :** \n"
+                    + f"⌔∮ `{spam_message}`",
+                )
+    elif BOTLOG:
+        if event.is_private:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ التكرار الوقتي **\n"
+                + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {sleeptimet} seconds and with {counter} **رسائل الـ   :** \n"
+                + f"⌔∮ `{spam_message}`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ التكرار الوقتي **\n"
+                + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع** {sleeptimet} **الثواني و مع** {counter} **رسائل الـ  ️ :** \n"
+                + f"⌔∮ `{spam_message}`",
+            )
+
+@l313l.ar_cmd(pattern="ايقاف التكرار ?(.*)")
+async def stopspamrz(event):
+    if gvarstatus("spamwork") is not None and gvarstatus("spamwork") == "true":
+        delgvar("spamwork")
+        return await edit_delete(event, "**⌔∮ تم بنجاح ايقاف التكرار **")
+    return await edit_delete(event, "**⌔∮ عذرا لم يتم تفعيل التكرار بالاصل**")
+
+@l313l.on(admin_cmd(pattern=r"تيست (\d+) (.+)"))
+async def nshr_joker(event):
+    seconds = int(event.pattern_match.group(1))
+    message = event.pattern_match.group(2)
+    reply = await event.get_reply_message()
+    await event.delete()
+    addgvar("spamwork", True)
+    await spam_function(event, reply, message, seconds, seconds, DelaySpam=True)
