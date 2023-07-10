16a82dc17fe8be9d9ed27f7ad0e55acbe005e76f
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-10 17:44:07 +0300

    Update ⁨ييالصيغ.py⁩

--- HuRe/plugins/ييالصيغ.py
+++ HuRe/plugins/ييالصيغ.py
@@ -401,13 +401,10 @@ async def pinterestAljoker(event):
             if 'image' in content_type:
                 img = Image.open(response.raw)
                 img.save("media.jpg", "JPEG", quality=100)
-                await event.client.send_message('me', file="media.jpg")
-            elif 'video' in content_type:
-                with open("media.mp4", 'wb') as file:
-                    for chunk in response.iter_content(chunk_size=1024):
-                        if chunk:
-                            file.write(chunk)
-                await event.client.send_message('me', file="media.mp4")
+                await event.client.send_file(event.chat_id, "media.jpg")
+            else:
+                await event.edit("** ᯽︙ هـذا لـيس رابـط صـورة**")
+                return
             await event.delete()
         else:
             await event.edit("** ᯽︙ حـدث خـطـأ أثـنـاء جـلـب الـوسـائـط مـن مـوقـع بـنـتـريـست**")
