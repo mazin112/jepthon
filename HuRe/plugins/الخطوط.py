70fb3284f510827b31058115571cb71d0bbac889
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-10 14:04:02 +0300

    Update ⁨الخطوط.py⁩

--- HuRe/plugins/الخطوط.py
+++ HuRe/plugins/الخطوط.py
@@ -63,7 +63,6 @@ async def reda(event):
     istshwesh = gvarstatus("tshwesh")
     if istshwesh:
         try:
-            await event.edit(f"||{event.message.message}||")
-            await event.edit(f"||{event.message.message}||")
+            await event.edit(f"~~{event.message.message}~~")
         except MessageIdInvalidError:
             pass
