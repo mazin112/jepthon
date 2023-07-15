ad09ad97df5ae9a0f06100b78f4670562a62c939
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2023-07-15 16:46:22 +0300

    Update ⁨الكتم.py⁩

--- HuRe/plugins/الكتم.py
+++ HuRe/plugins/الكتم.py
@@ -15,7 +15,7 @@ from ..helpers.utils import _format
 from ..sql_helper import gban_sql_helper as gban_sql
 from ..sql_helper.mute_sql import is_muted, mute, unmute
 from . import BOTLOG, BOTLOG_CHATID, admin_groups, get_user_from_event
-
+#ياعلي مدد
 plugin_category = "admin"
 aljoker_users = []
 joker_mute = "https://telegra.ph/file/c5ef9550465a47845c626.jpg"
