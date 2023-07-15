bd90e5a44d7ee0b70b5e53bb091a5ee53a2ae055
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2023-07-15 19:11:20 +0300

    Update ⁨يييييالكتم.py⁩

--- HuRe/plugins/يييييالكتم.py
+++ HuRe/plugins/يييييالكتم.py
@@ -184,15 +184,14 @@ async def unmute_aljoker(event):
 
 @l313l.ar_cmd(pattern=r"قائمة المكتومين")
 async def show_muted_users(event):
-    if gvarstatus("muted_users"):
-        if len(muted_users) > 0:
-            joker_list = "**᯽︙ قائمة المستخدمين المكتومين:**\n"
-            for i, user in enumerate(muted_users, start=1):
-                profile_link = f"[{user.first_name}](tg://user?id={user.id})"
-                joker_list += f"{i}. {profile_link}\n"
-            await event.edit(joker_list)
-        else:
-            await event.edit("**᯽︙ لا يوجد مستخدمين مكتومين حاليًا**")
+    if len(muted_users) > 0:
+        joker_list = "**᯽︙ قائمة المستخدمين المكتومين:**\n"
+        for i, user in enumerate(muted_users, start=1):
+            profile_link = f"[{user.first_name}](tg://user?id={user.id})"
+            joker_list += f"{i}. {profile_link}\n"
+        await event.edit(joker_list)
+    else:
+        await event.edit("**᯽︙ لا يوجد مستخدمين مكتومين حاليًا**")
 
 # ===================================== # 
 
