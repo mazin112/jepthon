164eaa866ad4f192bb263e001fc3eb143e04faf5
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2023-07-15 19:27:34 +0300

    Update ⁨يييييالكتم.py⁩

--- HuRe/plugins/يييييالكتم.py
+++ HuRe/plugins/يييييالكتم.py
@@ -24,8 +24,9 @@ joker_unmute = "https://telegra.ph/file/e9473ddef0b58cdd7f9e7.jpg"
 #=================== الكـــــــــــــــتم  ===================  #
 
 async def save_muted_users():
+    global muted_users
     await addgvar("muted_users", muted_users)
-
+    
 def mute_user(user):
     if user not in muted_users:
         muted_users.append(user)
