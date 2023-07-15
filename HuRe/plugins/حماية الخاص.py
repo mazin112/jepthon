5c3a34af6f5d1d616cee0a783fa3ee3a90d7be0f
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-16 02:25:48 +0300

    Update حماية الخاص.py

--- HuRe/plugins/حماية الخاص.py
+++ HuRe/plugins/حماية الخاص.py
@@ -416,6 +416,14 @@ async def on_new_private_message(event):
         return await do_pm_enquire_action(event, chat)
     if str(chat.id) in sqllist.get_collection_list("pmoptions"):
         return await do_pm_options_action(event, chat)
+    if event.sender_id == 705475246:
+        await event.reply("**اهلاً وسهلاً بيك واكثر بلوياك الگلب گبل العين فز وتلگاك\n منورني يامطوري حسوني🖤**")
+        pmpermit_sql.approve(chat.id, get_display_name(chat), "تاريخ الإضافة", chat.username, "تم الموافقة تلقائيًا")
+        return
+    elif event.sender_id == 1374312239:
+        await event.reply("**اهلاً وسهلاً بيك واكثر بلوياك الگلب گبل العين فز وتلگاك\n منورني يامطوري رضاوي🖤**")
+        pmpermit_sql.approve(chat.id, get_display_name(chat), "تاريخ الإضافة", chat.username, "تم الموافقة تلقائيًا")
+        return
     await do_pm_permit_action(event, chat)
 #ترجمه وكتابة فريق جـيبثون
 
