85a8a85b180821ab1f56b69f48493d0799760d0a
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-24 17:42:28 +0300

    Update تجميع نقاط.py

--- HuRe/plugins/تجميع نقاط.py
+++ HuRe/plugins/تجميع نقاط.py
@@ -16,6 +16,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 import re
 from telethon import events
 c = requests.session()
+bot_username = '@zmmbot'
 bot_username2 = '@A_MAN9300BOT'
 bot_username3 = '@MARKTEBOT'
 bot_username4 = '@qweqwe1919bot'
@@ -25,30 +26,31 @@ its_hussein = False
 its_reda = False
 its_joker = False
 #اياثارات الحسين
-@l313l.on(admin_cmd(pattern="تجميع"))
+
+@l313l.on(admin_cmd(pattern="(تجميع المليار|تجميع مليار)"))
 async def _(event):
     if HuRe[0] == "yes":
-        await event.edit("**᯽︙سيتم تجميع نقاط بوت المليار، قبل كل شيء تأكد من أنك قمت بالانضمام إلى القنوات الاشتراك الاجباري للبوت لتجنب حدوث أخطاء.**")
-        bot_username = '@zmmbot'
-        bot_entity = await l313l(GetFullUserRequest(bot_username))
-        channel_entity = bot_entity.user
-        await l313l.send_message(bot_username, '/start')
+        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+        channel_entity = await l313l.get_entity(bot_username)
+        await l313l.send_message('@zmmbot', '/start')
         await asyncio.sleep(5)
-        await l313l(JoinChannelRequest(bot_username))
-        msg0 = await l313l.get_messages(bot_username, limit=1)
+        msg0 = await l313l.get_messages('@zmmbot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(5)
-        msg1 = await l313l.get_messages(bot_username, limit=1)
+        msg1 = await l313l.get_messages('@zmmbot', limit=1)
         await msg1[0].click(0)
+
         chs = 1
         for i in range(100):
             if HuRe[0] == 'no':
                 break
             await asyncio.sleep(5)
-            list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+
+            list = await jepiq(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
-            if msgs.message.find('لا يوجد قنوات في الوقت الحالي، قم بتجميع النقاط بطريقة مختلفة') != -1:
-                await l313l.send_message(event.chat_id, "**لا توجد قنوات للبوت.**")
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await l313l.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
@@ -57,21 +59,21 @@ async def _(event):
                 except:
                     bott = url.split('/')[-1]
                     await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages(bot_username, limit=1)
+                msg2 = await l313l.get_messages('@zmmbot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
                 await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
             except:
-                await l313l.send_message(event.chat_id, "**خطأ، قد يكون تم حظرك.**")
+                await l313l.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
                 break
-        await l313l.send_message(event.chat_id, "**تم الانتهاء من التجميع!**")
-    else:
-        await event.edit("يجب الدفع لاستخدام هذا الأمر!")
+        await l313l.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
 
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+        
 @l313l.on(admin_cmd(pattern="(الغاء التجميع|الغاء تجميع)"))
 async def cancel_collection(event):
-    bot_username = event.pattern_match.group(2)
-    await l313l.send_message(bot_username, '/start')
+    await l313l.send_message('@zmmbot', '/start')
     await event.edit("** ᯽︙ تم الغاء التجميع من بوت المليار **")
     
 @l313l.on(admin_cmd(pattern="(تجميع الجوكر|تجميع جوكر)"))
