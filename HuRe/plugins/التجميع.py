097955cf7081376c5e36cd2ec96c52f4571899ac
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-25 22:59:31 +0300

    Update التجميع.py

--- HuRe/plugins/التجميع.py
+++ HuRe/plugins/التجميع.py
@@ -199,49 +199,46 @@ async def Hussein(event):
 
 @l313l.on(admin_cmd(pattern="(تجميع المليار|تجميع مليار)"))
 async def _(event):
-    if HuRe[0] == "yes":
-        await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
-        channel_entity = await l313l.get_entity(bot_username)
-        await l313l.send_message('@zmmbot', '/start')
-        await asyncio.sleep(5)
-        msg0 = await l313l.get_messages('@zmmbot', limit=1)
-        await msg0[0].click(2)
-        await asyncio.sleep(5)
-        msg1 = await l313l.get_messages('@zmmbot', limit=1)
-        await msg1[0].click(0)
+    await event.edit("**᯽︙سيتم تجميع النقاط من بوت المليار , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+    channel_entity = await l313l.get_entity(bot_username)
+    await l313l.send_message(bot_username, '/start')
+    await asyncio.sleep(4)
+    msg0 = await l313l.get_messages(bot_username, limit=1)
+    await msg0[0].click(2)
+    await asyncio.sleep(4)
+    msg1 = await l313l.get_messages(bot_username, limit=1)
+    await msg1[0].click(0)
 
-        chs = 1
-        for i in range(100):
-            if HuRe[0] == 'no':
-                break
-            await asyncio.sleep(5)
+    chs = 1
+    for i in range(100):
+        await asyncio.sleep(4)
 
-            list = await jepiq(GetHistoryRequest(peer=channel_entity, limit=1,
-                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
-            msgs = list.messages[0]
-            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await l313l.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
-                break
-            url = msgs.reply_markup.rows[0].buttons[0].url
-            try:
-                try:
-                    await l313l(JoinChannelRequest(url))
-                except:
-                    bott = url.split('/')[-1]
-                    await l313l(ImportChatInviteRequest(bott))
-                msg2 = await l313l.get_messages('@zmmbot', limit=1)
-                await msg2[0].click(text='تحقق')
-                chs += 1
-                await l313l.send_message("me", f"تم الاشتراك في {chs} قناة")
+        list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+             await l313l.send_message(event.chat_id, f"تم الانتهاء من التجميع")
+
+            break
+        url = msgs.reply_markup.rows[0].buttons[0].url
+        try:
+             try:
+                await l313l(JoinChannelRequest(url))
             except:
-                await l313l.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
-                break
-        await l313l.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
-
-    else:
-        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+                bott = url.split('/')[-1]
+                 await l313l(ImportChatInviteRequest(bott))
+            msg2 = await l313l.get_messages(bot_username, limit=1)
+            await msg2[0].click(text='تحقق')
+            chs += 1
+            await event.edit(f"تم الانضمام في {chs} قناة")
+        except:
+            msg2 = await l313l.get_messages(bot_username, limit=1)
+            await msg2[0].click(text='التالي')
+            chs += 1
+            await event.edit(f"القناة رقم {chs}")
+    await l313l.send_message(event.chat_id, "تم الانتهاء من التجميع")
         
-@l313l.on(admin_cmd(pattern="(الغاء التجميع|الغاء تجميع)"))
+@l313l.on(admin_cmd(pattern="(ايقاف التجميع|ايقاف تجميع)"))
 async def cancel_collection(event):
     await l313l.send_message('@zmmbot', '/start')
     await event.edit("** ᯽︙ تم الغاء التجميع من بوت المليار **")
