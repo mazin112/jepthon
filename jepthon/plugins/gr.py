2a46d0cf75e0cb0a95b1633ae1cac1159e191f11
mazin112 <120086917+mazin112@users.noreply.github.com>
2023-07-28 22:16:23 +0200

    Update gr.py

--- jepthon/plugins/gr.py
+++ jepthon/plugins/gr.py
@@ -4,7 +4,11 @@ from telethon.sessions import StringSession
 from telethon.tl.types import InputMessagesFilterEmpty
 import random
 
+API_ID = 21889593
+API_HASH = '761af3ce5e7b8d4159a127fe43571fa9'
+STRING_SESSION = '1BJWap1wBu73PSV26UxjZy6-akc3brvATphrRbNp6R3Owu6aSdWVRbq_CnqBTq7wHglWLMktt8eyleVSo6-_BerUUWPyHsWSmYip7YTnKlOyd5xwiYF8DMZTY08xJcb-2XWu20GzOP5LvM0ZWAIpbKcDBV90d8-7C5tILczKpf21kF79tqBPv4ggL-3F9lBrJRNsh8SrRetX4ngvGNf2yG9NiVjeXJ0HoozYXYkq0syI56f7B6JJo_wGH4nI_o0uzolvHX2h6Fe99ipkJo-h-hDnFccfU-2dXXiTqgS8m4lxi0st4jH7TcRVekVbppl2FxYl8oR-tsbcwqW48OSBk4QLKOjftqxk='
 
+client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 # Start the client
 async def main():
