22202fc70d8a43ab0f520077b94ae19c1fbb8057
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-07-28 19:30:18 +0300

    Update اوامر الكروب.py

--- HuRe/plugins/اوامر الكروب.py
+++ HuRe/plugins/اوامر الكروب.py
@@ -770,14 +770,19 @@ async def Hussein(event):
                 await event.reply(f"**᯽︙ عذرًا {aljoker_profile}، يُرجى عدم إرسال الرسائل التي تحتوي على إيموجي المميز. لديك {emoji_warnings[sender_id]} تحذيرات متبقية.**")
             else:
                 await event.delete()
-                await event.client(
-                    EditBannedRequest(
-                        event.chat_id,
-                        event.sender_id,
-                        ChatBannedRights(until_date=None, send_messages=True),
+                try:
+                    chat = await event.client.get_input_entity(event.chat_id)
+                except ValueError:
+                    pass
+                else:
+                    await event.client(
+                        EditBannedRequest(
+                            chat,
+                            event.sender_id,
+                            ChatBannedRights(until_date=None, send_messages=True),
+                        )
                     )
-                )
-                await event.reply(f"**᯽︙ تم تقييدك {aljoker_profile} من إرسال الرسائل بسبب ارسالك للإيموجي المُميز بكثرة 🖤**")
+                    await event.reply(f"**᯽︙ تم تقييدك {aljoker_profile} من إرسال الرسائل بسبب ارسالك للإيموجي المُميز بكثرة 🖤**")
 
 @l313l.ar_cmd(pattern="المميز تفعيل", require_admin=True, groups_only=True)
 async def enable_emoji_blocker(event):
